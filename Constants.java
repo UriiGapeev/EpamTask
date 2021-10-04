9f553e3ab31dfdb97f5bace119dee02ff0b44011
UriiGapeev <91692875+UriiGapeev@users.noreply.github.com>
2021-10-04 22:57:12 +0300

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package calculation.by.home.task;
+
+public final class Constants {
+    public static final double DEGREE_FROM_FOLRMULA_PERIMETR = 2;
+    public static final double NUMBER_FROM_FORMULA_SQUARE = 0.5;
+    private Constants() {
+    }
+}
